4feea4ec9412d31acbc0b161b4981395057904c2
SrinivasanNikhil <nikhil.srinivasan@gmail.com>
2015-09-08 22:30:01 -0400

    Controller
    Created a controller class to start the game.

---
+++ src/Controller.java
@@ -0,0 +1,14 @@
+
+public class Controller {
+
+	public Controller() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Table table1 = new Table();
+
+	}
+
+}
